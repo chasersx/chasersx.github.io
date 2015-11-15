a983a8d145194566405a32b555040bb4ba8e49fe
Dustin Lafley <chasersx@gmail.com>
2015-11-15 16:14:18 -0600

    Adding Resume
    adding resume files

---
+++ js/resumeBuilder.js
@@ -0,0 +1,58 @@
+//var name = "Dustin Lafley";
+//var role = "Web Developer";
+//var formattedName = HTMLheaderName.replace("%data%", name);
+//var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+var skills = ["C#", "SQL Server", "HTML"]
+
+var bio = {
+	"name" : "Dustin",
+	"role" : "Front-end Ninja",
+	"contactInfo" : "dustin.lafley@gmail.com",
+	"pictureURL" : "images/fry.jpg",
+	"welcomeMessage" : "Hello",
+	"age" : 35,
+	"skills" : skills
+};
+
+var work = 
+{
+	"jobs" :
+	[
+		{
+			"jobPosition" : "Applications Developer",
+			"employer" : "Schlumberger",
+			"yearsWorked" : 10,
+			"location" : "Houston",
+		}
+	]
+}
+//work.jobPosition = "Applications Developer";
+//work.employer = "Schlumberger";
+//work.yearsWorked = 10;
+//work.location = "Houston"
+
+var education = {
+	"schools" : 
+	[
+		{
+			"name" : "University of Houston",
+			"city" : "Houston",
+			"degree" : "BS",
+			"major" : "Computer Science",
+			"minor" : "Math",
+			"years" : "2001-2005"
+		},
+		{
+			"name" : "Lone Star College",
+			"city" : "Houston",
+			"years" : "1999-2001"
+		}
+	]
+}
+
+
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+//$("#main").append(work.jobPosition);
+//$("#main").append(edu["name"]);
